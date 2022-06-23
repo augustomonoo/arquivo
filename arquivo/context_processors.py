eb7fdaf99ea875e8236e4ff6a9671435efe40896
Carlos Augusto Monoo Pereira Barbosa <augustomonoo@gmail.com>
2022-06-23 19:05:30 -0300

    Usando context_processor para injetar os links da navbar

--- arquivo/context_processors.py
+++ arquivo/context_processors.py
@@ -1,11 +1,26 @@
+from django.urls import reverse_lazy
 from arquivo.models import Documento, Arquivo, TipoDeDocumento, Cliente
 
 
-def arquivo(request):
-    """Adiciona models do app ao context dos templates para buscar as urls deles"""
+def nav_links(request) -> dict:
+    return {
+        "nav_links": (
+            {"href": reverse_lazy("main_index"), "text": "Início"},
+            "Documentos",
+            {"href": Documento.get_listar_url(), "text": "Consultar documentos"},
+            "Arquivo",
+            {"href": Arquivo.get_criar_url(), "text": "Novo Arquivo"},
+            {"href": Arquivo.get_listar_url(), "text": "Consultar arquivos"},
+        ),
+    }
+
+
+def arquivo(request) -> dict:
+    """Contexto usado por todas as views na aplicação"""
     return {
         "Arquivo": Arquivo,
         "Documento": Documento,
         "TipoDeDocumento": TipoDeDocumento,
         "Cliente": Cliente,
+        **nav_links(request),
     }
