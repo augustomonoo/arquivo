2553da5fb1bf8170f46d135b3d7d2d80cc32d210
Carlos Augusto Monoo Pereira Barbosa <augustomonoo@gmail.com>
2022-06-23 18:02:13 -0300

    Convertido form de pesquisa para uma classe reutilizavel

--- arquivo/forms.py
+++ arquivo/forms.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from dataclasses import field
 from django import forms
 from arquivo.models import Arquivo, Documento
@@ -13,15 +14,33 @@ class ArquivoForm(forms.ModelForm):
         return super().save(*args, **kwargs)
 
 
-class ConsultaForm(forms.Form):
+class ConsultaBaseForm(forms.Form):
     termos = forms.CharField(max_length=255)
+
+    # lista de tuplas (campo, operador)
+    # campo: nome do campo no modelo (atual ou relacionado)
+    # operador: "icontains", "iexact", etc
+    campos_de_pesquisa = []
 
     def search(self, queryset: QuerySet) -> QuerySet:
         termos = self.cleaned_data["termos"]
         termos = termos.split()
+        q = Q()
         for termo in termos:
-            queryset = queryset.filter(Q(conteudo__icontains=termo))
-        return queryset
+            for campo, operador in self.campos_de_pesquisa:
+                q = q | Q(**{f"{campo}__{operador}": termo})
+        return queryset.filter(q)
+
+    @abstractmethod
+    def get_campos_de_pesquisa(self):
+        return self.campos_de_pesquisa
+
+
+class ConsultaDocumentoForm(ConsultaBaseForm):
+    campos_de_pesquisa = [
+        ("conteudo", "icontains"),
+        ("cliente__razao_social", "icontains"),
+    ]
 
 
 class DocumentoForm(forms.ModelForm):
