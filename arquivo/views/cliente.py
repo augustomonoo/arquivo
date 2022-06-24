5305d1d880d5fbc115e0c2394f116397aa9fe120
Luiz Guilherme Monoo Pereira Barbosa <luizmonoo@gmail.com>
2022-06-24 20:53:11 -0300

    Criação da view views.cliente.cliente_novo, faltam os templates

---
+++ arquivo/views/cliente.py
@@ -0,0 +1,17 @@
+from audioop import reverse
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from arquivo.forms import ClienteForm
+from arquivo.models import Cliente
+
+
+def cliente_novo(request) -> HttpResponse:
+    form = ClienteForm(request.POST or None)
+    if form.is_valid():
+        cliente = form.save()
+        return redirect(cliente.get_editar_url())
+    contexto = {
+        "cliente": cliente,
+        "form": form,
+    }
+    return render(request, "arquivo/cliente/novo.html", contexto)
