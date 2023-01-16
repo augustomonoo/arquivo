b1170eb4595b0303a7d98eaafea403a43f7a3cbc
Luiz Guilherme Monoo Pereira Barbosa <luizmonoo@gmail.com>
2023-01-16 17:33:22 -0300

    Modelo cliente ajustado para ser similar ao modelo FIN_CLIENTES do sistema antigo

--- arquivo/models/cliente.py
+++ arquivo/models/cliente.py
@@ -3,7 +3,22 @@ from django.urls import reverse_lazy
 
 
 class Cliente(models.Model):
-    razao_social = models.CharField(max_length=512, verbose_name="razão social")
+    data_cadastro = models.DateTimeField(null=True)
+    nome = models.CharField(max_length=120, null=True)
+    telefone1 = models.CharField(max_length=16, null=True)
+    telefone_contato = models.CharField(max_length=30, null=True)
+    cep = models.CharField(max_length=9, null=True)
+    tipo_pessoa = models.CharField(max_length=1, default="F")
+    cidade = models.CharField(max_length=30, null=True)
+    sigla_estado = models.CharField(max_length=2, null=True)
+    cnpj = models.CharField(max_length=20, null=True)
+    inscricao_estadual = models.CharField(max_length=20, null=True)
+    razao_social = models.CharField(
+        max_length=512, verbose_name="razão social", null=True
+    )
+    atividade = models.CharField(max_length=250, null=True)
+    endereco = models.CharField(max_length=60, null=True)
+    bairro = models.CharField(max_length=30, null=True)
 
     def __str__(self) -> str:
         return f"{self.razao_social}"
