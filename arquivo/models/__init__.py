dae38e6fd94c1a36c9a74f0285e7d2b764bdb8d1
Luiz Guilherme Monoo Pereira Barbosa <luizmonoo@gmail.com>
2023-01-16 02:51:17 -0300

    Modelo "Historico" criado, nome no sistema antigo CON_EVENTO

--- arquivo/models/__init__.py
+++ arquivo/models/__init__.py
@@ -1,8 +1,4 @@
-from django.db import models
-from django.urls import reverse_lazy
-
 from .cliente import Cliente  # fin_clientes
 from .documento import Documento  # lan_lancaamento
+from .historico import Historico  # con_evento
 from .tipo_de_documento import TipoDeDocumento  # cad_descricao
-
-# from .historico import Historico  # con_evento
---
+++ arquivo/models/historico.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+
+class Historico(models.Model):
+    """
+    Gerado e gerenciado automaticamente, nao pode ser alterado por usuarios.
+    """
+
+    user = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
+    descricao = models.CharField(max_length=100)
+    data = models.DateTimeField()
+    formulario = models.CharField(max_length=80)  # qual aba gerou isso
+    # copia do nome do usuario que gerou o historico
+    nome_user = models.CharField(max_length=80)
