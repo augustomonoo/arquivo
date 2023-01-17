e54b6b152976c19d2e17ee8764bf16480bd05134
Luiz Guilherme Monoo Pereira Barbosa <luizmonoo@gmail.com>
2023-01-17 00:54:25 -0300

    Funcoes para importar o banco de dados antigo

--- arquivo/management/commands/import_old_db.py
+++ arquivo/management/commands/import_old_db.py
@@ -1,3 +1,5 @@
+import json
+
 from django.core.management.base import BaseCommand
 from django.core.management.base import CommandError
 
@@ -7,6 +9,46 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("arquivo_json", type=str)
+        parser.add_argument("tabela_alvo", type=str)
+
+    def importar_usuario(self, table: list[dict[str, str]]) -> None:
+        pass
+
+    def importar_historico(self, table: list[dict[str, str]]) -> None:
+        pass
+
+    def importar_cliente(self, table: list[dict[str, str]]) -> None:
+        pass
+
+    def importar_tipodocumento(self, table: list[dict[str, str]]) -> None:
+        pass
+
+    def importar_documento(self, table: list[dict[str, str]]) -> None:
+        pass
 
     def handle(self, *args, **options):
-        print(options["arquivo_json"])
+        file_path = options["arquivo_json"]
+        with open(file_path, "r") as f:
+            old_database = json.loads(f.read())
+            tabela_usuarios = old_database["FIN_FUNCIONARIOS.sql"]
+            tabela_historico = old_database["CON_EVENTO.sql"]
+            tabela_clientes = old_database["FIN_CLIENTES.sql"]
+            tabela_tipo_doc = old_database["CAD_DESCRICAO.sql"]
+            tabela_documentos = old_database["LAN_LANCAMENTO.sql"]
+        match options.get("tabela_alvo", None):
+            case "usuarios":
+                importar_usuario(tabela_usuarios)
+            case "historico":
+                importar_historico(tabela_historico)
+            case "cliente":
+                importar_cliente(tabela_clientes)
+            case "tipo_de_documento":
+                importar_tipodocumento(tabela_tipo_doc)
+            case "documento":
+                importar_documento(tabela_documentos)
+            case _:
+                importar_usuario(tabela_usuarios)
+                importar_historico(tabela_historico)
+                importar_cliente(tabela_clientes)
+                importar_tipodocumento(tabela_tipo_doc)
+                importar_documento(tabela_documentos)
