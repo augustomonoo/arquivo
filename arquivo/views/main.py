27d7a07841da42f4c1f5f22ccc3fdea86e726e38
Luiz Guilherme Monoo Pereira Barbosa <luizmonoo@gmail.com>
2023-01-30 02:40:04 -0300

    A view index requer login

--- arquivo/views/main.py
+++ arquivo/views/main.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
 from arquivo.models import Documento
 
 
+@login_required
 def index(request) -> HttpResponse:
     ultimos_lancamentos = Documento.objects.order_by("-data_arquivo")[:10]
     contexto = {"ultimos_lancamentos": ultimos_lancamentos}
