960fb70581b87f453cb7adbcaa73d43227f33d3d
Luiz Guilherme Monoo Pereira Barbosa <luizmonoo@gmail.com>
2023-02-02 02:12:27 -0300

    Criacao de dois context_processors para inserir os parametros GET para serem utilizados com paginadores

--- arquivo/context_processors.py
+++ arquivo/context_processors.py
@@ -23,6 +23,21 @@ def nav_links(request) -> dict:
     }
 
 
+def paginator_get_str(request) -> str:
+    arguments = (
+        {k: v for k, v in request.GET.items() if k != "page"} if request.GET else {}
+    )
+    return (
+        ("&".join(["=".join([k, v]) for k, v in arguments.items()]) + "&")
+        if arguments
+        else ""
+    )
+
+
+def paginator_get(request) -> dict:
+    return {k: v for k, v in request.GET.items() if k != "page"} if request.GET else {}
+
+
 def arquivo(request) -> dict:
     """Contexto usado por todas as views na aplicação"""
     return {
@@ -31,4 +46,6 @@ def arquivo(request) -> dict:
         "TipoDeDocumento": TipoDeDocumento,
         "Cliente": Cliente,
         **nav_links(request),
+        "paginator_get_str": paginator_get_str(request),
+        "paginator_get": paginator_get(request),
     }
