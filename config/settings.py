35d524119094151e1e8c080f816d3a7e64ea1702
Luiz Guilherme Monoo Pereira Barbosa <luizmonoo@gmail.com>
2023-02-05 11:33:45 -0300

    Ajuste do caminho do STATIC_ROOT caso PUBLIC_PATH nao esteja definido

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.environ.get("PUBLIC_PATH", "") + "/static/"
+STATIC_ROOT = os.environ.get("PUBLIC_PATH", ".") + "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
