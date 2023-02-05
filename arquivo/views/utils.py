937f578fd9eae84706cb407b995ff06fcada63ab
Luiz Guilherme Monoo Pereira Barbosa <luizmonoo@gmail.com>
2023-02-05 16:17:04 -0300

    Implementacao da funcao utils.get_range_objects que retorna todos os objetos que estariam contidos em uma paginacao

--- arquivo/views/utils.py
+++ arquivo/views/utils.py
@@ -1,8 +1,13 @@
-from typing import Any
 from django.core.paginator import Paginator
+from typing import Any
 
 
-def paginate(queryset, page_size: int = 25, page_number: int = 1) -> dict[str, Any]:
+__PAGINATOR_DEFAULT_SIZE__: int = 25
+
+
+def paginate(
+    queryset, page_size: int = __PAGINATOR_DEFAULT_SIZE__, page_number: int = 1
+) -> dict[str, Any]:
     paginator = Paginator(queryset, page_size)
     page = paginator.get_page(page_number)
     return {
@@ -10,3 +15,25 @@ def paginate(queryset, page_size: int = 25, page_number: int = 1) -> dict[str, A
         "paginator": paginator,
         "page_obj": page,
     }
+
+
+def get_range_objects(
+    queryset,
+    object_per_page: int = __PAGINATOR_DEFAULT_SIZE__,
+    start_page: int = 1,
+    end_page: int = None,
+):  # sourcery skip: remove-unnecessary-cast
+    try:
+        start_page = int(start_page)
+    except ValueError:
+        start_page = 0
+    try:
+        end_page = int(end_page)
+    except ValueError:
+        end_page = 0
+
+    start = start_page * object_per_page
+    if end_page:
+        end = end_page * object_per_page
+        return {"object_list": queryset[start:end]}
+    return {"object_list": queryset[start:]}
