8680846baf83eaa5b0e4417eaca35c6270c3ce31
Luiz Guilherme Monoo Pereira Barbosa <luizmonoo@gmail.com>
2024-09-27 20:54:19 -0300

    Removido o context_processor "nav_links".

--- arquivo/context_processors.py
+++ arquivo/context_processors.py
@@ -1,26 +1,4 @@
-from arquivo.models import Cliente
-from arquivo.models import Documento
-from arquivo.models import TipoDeDocumento
-from django.urls import reverse_lazy
-
-
-def nav_links(request) -> dict:
-    return {
-        "nav_links": (
-            {"href": reverse_lazy("main_index"), "text": "Início"},
-            "Documentos",
-            {"href": Documento.get_listar_url(), "text": "Consultar documentos"},
-            {"href": Documento.get_criar_url(), "text": "Novo Lançamento"},
-            # "Arquivos",
-            # {"href": Arquivo.get_criar_url(), "text": "Novo Arquivo"},
-            # {"href": Arquivo.get_listar_url(), "text": "Consultar arquivos"},
-            "Clientes",
-            {"href": Cliente.get_listar_url(), "text": "Consultar clientes"},
-            {"href": Cliente.get_criar_url(), "text": "Novo Cliente"},
-            "Conta",
-            {"href": reverse_lazy("logout"), "text": "Sair"},
-        ),
-    }
+from arquivo.models import Cliente, Documento, TipoDeDocumento
 
 
 def paginator_get_str(request) -> str:
@@ -45,7 +23,6 @@ def arquivo(request) -> dict:
         "Documento": Documento,
         "TipoDeDocumento": TipoDeDocumento,
         "Cliente": Cliente,
-        **nav_links(request),
         "paginator_get_str": paginator_get_str(request),
         "paginator_get": paginator_get(request),
     }
