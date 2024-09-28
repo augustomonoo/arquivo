ef621c80c415dc54dcec9ead6fa874b9b93c3855
Luiz Guilherme Monoo Pereira Barbosa <luizmonoo@gmail.com>
2024-09-27 21:06:59 -0300

    Ajustado para que menu retorne um dicionario com objetos NavLinks

--- arquivo/templatetags/componentes.py
+++ arquivo/templatetags/componentes.py
@@ -1,24 +1,33 @@
+from typing import NamedTuple, Optional
+
 from django import template
 from django.urls import reverse_lazy
+
 from arquivo.models import Documento, Cliente
 
 register = template.Library()
 
+
+class NavLink(NamedTuple):
+    text: str
+    href: Optional[str] = None
+
+
 @register.inclusion_tag("arquivo/componentes/menu.html")
-def menu() -> dict:
+def menu() -> dict[str, list[NavLink]]:
     return {
-        "nav_links": (
-            {"href": reverse_lazy("main_index"), "text": "Início"},
-            "Documentos",
-            {"href": Documento.get_listar_url(), "text": "Consultar documentos"},
-            {"href": Documento.get_criar_url(), "text": "Novo Lançamento"},
+        "nav_links": [
+            NavLink(text="Início", href=reverse_lazy("main_index")),
+            NavLink(text="Documentos"),
+            NavLink(text="Consultar Documentos", href=Documento.get_listar_url()),
+            NavLink(text="Novo Lançamento", href=Documento.get_criar_url()),
             # "Arquivos",
             # {"href": Arquivo.get_criar_url(), "text": "Novo Arquivo"},
             # {"href": Arquivo.get_listar_url(), "text": "Consultar arquivos"},
-            "Clientes",
-            {"href": Cliente.get_listar_url(), "text": "Consultar clientes"},
-            {"href": Cliente.get_criar_url(), "text": "Novo Cliente"},
-            "Conta",  # TODO: trocar o link de logout para um formulario de logout
-            {"href": reverse_lazy("logout"), "text": "Sair"},
-        ),
+            NavLink(text="Clientes"),
+            NavLink(text="Consultar Clientes", href=Cliente.get_listar_url()),
+            NavLink(text="Novo Cliente", href=Cliente.get_criar_url()),
+            NavLink(text="Conta"),  # TODO: trocar o link de logout para um formulario de logout
+            NavLink(text="Sair", href=reverse_lazy("logout")),
+        ],
     }
