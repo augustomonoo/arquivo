6f6272b182801b555a35d61ddaca1025e2f470a6
Luiz Guilherme Monoo Pereira Barbosa <luizmonoo@gmail.com>
2024-09-27 21:24:32 -0300

    Ajustando o novo NavLink para suportar fazer referencia a um componente de formulario

--- arquivo/templatetags/componentes.py
+++ arquivo/templatetags/componentes.py
@@ -8,29 +8,36 @@ from arquivo.models import Documento, Cliente
 register = template.Library()
 
 
+class HRef(str):
+    pass
+
+
 class NavLink(NamedTuple):
     text: str
-    href: Optional[str] = None
+    action: Optional[HRef] = None
+
+    def href(self) -> Optional[HRef]:
+        return self.action
 
     def is_link(self) -> bool:
-        return self.href is not None
+        return self.action is not None
 
 
 @register.inclusion_tag("arquivo/componentes/menu.html")
 def menu() -> dict[str, list[NavLink]]:
     return {
         "nav_links": [
-            NavLink(text="Início", href=reverse_lazy("main_index")),
+            NavLink(text="Início", action=HRef(reverse_lazy("main_index"))),
             NavLink(text="Documentos"),
-            NavLink(text="Consultar Documentos", href=Documento.get_listar_url()),
-            NavLink(text="Novo Lançamento", href=Documento.get_criar_url()),
+            NavLink(text="Consultar Documentos", action=HRef(Documento.get_listar_url())),
+            NavLink(text="Novo Lançamento", action=HRef(Documento.get_criar_url())),
             # "Arquivos",
             # {"href": Arquivo.get_criar_url(), "text": "Novo Arquivo"},
             # {"href": Arquivo.get_listar_url(), "text": "Consultar arquivos"},
             NavLink(text="Clientes"),
-            NavLink(text="Consultar Clientes", href=Cliente.get_listar_url()),
-            NavLink(text="Novo Cliente", href=Cliente.get_criar_url()),
+            NavLink(text="Consultar Clientes", action=HRef(Cliente.get_listar_url())),
+            NavLink(text="Novo Cliente", action=HRef(Cliente.get_criar_url())),
             NavLink(text="Conta"),  # TODO: trocar o link de logout para um formulario de logout
-            NavLink(text="Sair", href=reverse_lazy("logout")),
+            NavLink(text="Sair", action=HRef(reverse_lazy("logout"))),
         ],
     }
